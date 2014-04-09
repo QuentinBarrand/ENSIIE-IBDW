f6c9c5efcea8eacc3165859e43bfa61cdcb43f3c
Quentin Barrand <quentin@quba.fr>
2014-04-09 23:08:34 +0200

    Suppression de copier-coller de code.

--- views/EvenementsLayout.php
+++ views/EvenementsLayout.php
@@ -11,70 +11,59 @@
 
 	date_default_timezone_set('UTC');
 
-?>
+	function printTable($type, $user, $data) {
+		echo '<table class="table table-striped">';
+		echo '<thead>';
 
-<h2>Evènements à venir</h2>
-<table class="table table-striped">
-<?php
-	echo '<thead>';
-
-	echo '<tr>';
-	echo '<th>Nom</th>';
-	echo '<th>Date et heure</th>';
-	echo '<th>Lieu</th>';
-	echo '</tr>';
-
-	echo '</thead>';
-	echo '<tbody>';
-
-	foreach($data['content'] as $row) {
-		$epoch_evenement = strtotime($row['heuredate']);
-
-		if(time() < $epoch_evenement) {
-			echo '<tr>';
-			
-			echo '<td>' . $row['nom'] . '</td>';
-			echo '<td>' . date('d / m / Y -  H:i:s', strtotime($row['heuredate'])) . '</td>';
-			echo '<td>' . $row['lieu'] . '</td>';
-
-			echo '</tr>';
+		echo '<tr>';
+		echo '<th>Nom</th>';
+		echo '<th>Date et heure</th>';
+		echo '<th>Lieu</th>';
+		
+		if($user['authenticated']) {
+			echo '<th>Statut</th>';	
 		}
+
+		echo '</tr>';
+
+		echo '</thead>';
+		echo '<tbody>';
+
+		foreach($data['content'] as $row) {
+			$epoch_evenement = strtotime($row['heuredate']);
+
+			if($type == 'a_venir')
+				$condition = time() > $epoch_evenement;
+			else
+				$condition = time() < $epoch_evenement;
+
+			if($condition) {
+				echo '<tr>';
+				
+				echo '<td>' . $row['nom'] . '</td>';
+				echo '<td>' . date('d / m / Y -  H:i:s', $epoch_evenement) . '</td>';
+				echo '<td>' . $row['lieu'] . '</td>';
+
+				if($user['authenticated']) {
+					echo '<td>';
+					echo '<span class="label label-danger">Pas assez de choristes</span>';
+					echo '</td>';
+				}
+
+				echo '</tr>';
+			}
+		}
+
+		echo '</tbody>';
+		echo '</table>';
 	}
 
-	echo '</tbody>';
+
+	echo '<h2>Evènements à venir</h2>';
+	printTable('a_venir', $user, $data);
+	
+	echo '<h2>Evènements passés</h2>';
+	printTable('passes', $user, $data);
 ?>
-</table>
-
-<h2>Evènements passés</h2>
-<table class="table table-striped">
-<?php
-	echo '<thead>';
-
-	echo '<tr>';
-	echo '<th>Nom</th>';
-	echo '<th>Date et heure</th>';
-	echo '<th>Lieu</th>';
-	echo '</tr>';
-
-	echo '</thead>';
-	echo '<tbody>';
-
-	foreach($data['content'] as $row) {
-		$epoch_evenement = strtotime($row['heuredate']);
-
-		if(time() > $epoch_evenement) {
-			echo '<tr>';
-			
-			echo '<td>' . $row['nom'] . '</td>';
-			echo '<td>' . date('d / m / Y -  H:i:s', $epoch_evenement) . '</td>';
-			echo '<td>' . $row['lieu'] . '</td>';
-
-			echo '</tr>';
-		}
-	}
-
-	echo '</tbody>';
-?>
-</table>
 
 <?php echo($footer); ?>
