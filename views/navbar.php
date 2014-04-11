47c9317aa01448050bff2b79608502fd373153c2
Thomas Sarboni <max-k@post.com>
2014-04-11 11:31:22 +0200

    Correcttion du lien MyChorus: retrait du # inutile

--- views/navbar.php
+++ views/navbar.php
@@ -7,7 +7,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="/#">MyChorus</a>
+      <a class="navbar-brand" href="/">MyChorus</a>
     </div>
     <div class="collapse navbar-collapse">
       <ul class="nav navbar-nav">
