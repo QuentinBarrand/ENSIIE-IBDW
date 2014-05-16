9fafb88933aaa8d9be04d4679544d75222f11616
Thomas Sarboni <max-k@post.com>
2014-05-16 12:00:27 +0200

    Extraction des requêtes controlleur inscriptions

--- controllers/Inscriptions.php
+++ controllers/Inscriptions.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once 'model/Inscriptions.php';
+
 class Inscriptions {
 
     // GET /inscriptions
@@ -16,41 +18,14 @@ class Inscriptions {
             return;
         }
 
-        // On cherche les validations du webmaster par defaut
-        //    $type = 0;
-        // Si tresorier, on cherche les validations du tresorier
-        //    if($user['responsabilite'] == 2)
-        //        $type = 1;
-
-        // Connexion à la base de données
         try {
-            $db = Flight::db();
+            list($status, $result) = I_Queries::getInscriptions();
+            $data['success'] = $status;
+            $data['content'] = $result;
         }
         catch(PDOException $e) {
-            $db = null;
             $data['success'] = false;
-            $data['error'] = 'Connexion à la base de données impossible (' . $e->getMessage() . ').';
-        }
-
-        $sql = 'SELECT nom, prenom, typeVoix, typeInscription, idInscription, validation
-                FROM Choriste
-                NATURAL JOIN Voix 
-                NATURAL JOIN Inscription
-                WHERE validation < 2;';
-
-        if($db) {
-            try {
-                $query = $db->prepare($sql);
-                
-                $query->execute();
-
-                $data['success'] = true;
-                $data['content'] = $query->fetchAll();
-            }
-            catch(PDOException $e) {
-                $data['success'] = false;
-                $data['error'] = 'Erreur lors de l\'exécution de la requête (' . $e->getMessage() . ').';
-            }
+            $data['error'] = 'Erreur lors de l\'exécution de la requête (' . $e->getMessage() . ').';
         }
 
         // Header
@@ -98,34 +73,19 @@ class Inscriptions {
             return;
         }
 
-        // Connexion à la base de données
-        try {
-            $db = Flight::db();
-        }
-        catch(PDOException $e) {
-            $db = null;
-        }
-
         // Detection du type de validation (Webmaster/Tresorier)
         $type = 1;
         if($user['responsabilite'] == 2)
             $type = 2;
 
-        $sql = 'UPDATE Inscription
-                SET validation = ' . $type . '
-                WHERE idInscription = ' . $inscriptionId . ';';
-
         // Execution de la requête SQL
-        if($db) {
-            try {
-                $query = $db->prepare($sql);
-                $query->execute();
-                $data['success'] = true;
-            }
-            catch(PDOException $e) {
-                $data['success'] = false;
-                $data['error'] = 'Erreur lors de l\'exécution de la requête (' . $e->getMessage() . ').';
-            }
+        try {
+            list($status, $result) = I_Queries::updateInscription($type, $inscriptionId);
+            $data['success'] = $status;
+        }
+        catch(PDOException $e) {
+            $data['success'] = false;
+            $data['error'] = 'Erreur lors de l\'exécution de la requête (' . $e->getMessage() . ').';
         }
 
         // Finalement on reditige vers la liste des validations
@@ -138,31 +98,16 @@ class Inscriptions {
      * 1: 2nde validation (trésorier)
      */
     function getCount($type) {
-        try {
-            $db = Flight::db();
-        }
-        catch(PDOException $e) {
-            $db = null;
-        }
-
-        $sql = 'SELECT count(*)
-                FROM Choriste
-                NATURAL JOIN Inscription
-                WHERE validation = ' . $type . ';';
 
         $count = 0;
 
-        if($db) {
-            try {
-                $query = $db->prepare($sql);
-                $query->execute();
-
-                $result = $query->fetch();
-                if($result[0])
-                    $count = $result[0];
-            }
-            catch(PDOException $e) { }
+        try {
+            list($status, $result) = I_Queries::getInscriptionCountByType($type);
+            $data['success'] = $status;
+            if($result[0])
+                $count = $result[0];
         }
+        catch(PDOException $e) { }
 
         return $count;
     }
---
+++ model/Inscriptions.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once 'Generic.php';
+
+class I_Queries {
+
+    /* --- INSCRIPTIONS RELATED QUERIES --- */
+
+    function getInscriptions() {
+
+        $fetchall = True;
+
+        $sql = 'SELECT nom, prenom, typeVoix, typeInscription, idInscription, validation
+                FROM Choriste
+                NATURAL JOIN Voix
+                NATURAL JOIN Inscription
+                WHERE validation > 2;';
+
+        list($success, $result) = Query::execute($sql, $fetchall);
+        return array($success, $result);
+
+    }
+
+    function getInscriptionCountByType($type) {
+
+        $fetchall = True;
+
+        $sql = 'SELECT count(*)
+                FROM Choriste
+                NATURAL JOIN Inscription
+                WHERE validation = ' . $type . ';';
+
+        list($success, $result) = Query::execute($sql, $fetchall);
+        return array($success, $result);
+
+    }
+
+    function updateInscription($type, $id) {
+
+        $fetchall = True;
+
+        $sql = 'UPDATE Inscription
+                SET validation = ' . $type . '
+                WHERE idInscription > ' . $id . ';';
+
+        list($success, $result) = Query::execute($sql, $fetchall);
+        return array($success, $result);
+
+    }
+
+}
